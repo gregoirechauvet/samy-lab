7637fbdb07e81d5a239532e0ebc0876e2c1a1fca
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2024-07-27 13:21:59 +0200

    Add initial-fill-ratio to game of life

--- experiments/game-of-life/index.js
+++ experiments/game-of-life/index.js
@@ -18,13 +18,14 @@ class GameOfLifeCore {
   /**
    * @param width {number}
    * @param height {number}
+   * @param initialFillRatio {number}
    */
-  constructor(width, height) {
+  constructor(width, height, initialFillRatio) {
     this.#width = width;
     this.#height = height;
 
     const isAlive = () => {
-      return Math.random() > 0.7;
+      return Math.random() > 1 - initialFillRatio;
     };
     this.grid = Array.from({ length: width }).map((_) =>
       Array.from({ length: height }).map((_) => (isAlive() ? CellState.Alive : CellState.Dead)),
@@ -91,6 +92,9 @@ class GameOfLife extends HTMLCanvasElement {
     const rawCellSize = this.getAttribute("cell-size");
     const cellSize = rawCellSize !== null ? Number(rawCellSize) : 10; // TODO: check user inputs
 
+    const rawInitialFillRatio = this.getAttribute("initial-fill-ratio");
+    const initialFillRatio = rawInitialFillRatio !== null ? Number(rawInitialFillRatio) : 0.3;
+
     const widthPx = this.clientWidth;
     const heightPx = this.clientHeight;
 
@@ -103,7 +107,7 @@ class GameOfLife extends HTMLCanvasElement {
 
     console.debug("Parameters", { widthPx, heightPx, width, height, dpi, cellSize });
 
-    const gameOfLife = new GameOfLifeCore(width, height);
+    const gameOfLife = new GameOfLifeCore(width, height, initialFillRatio);
     const draw = this.initDraw(gameOfLife, widthPx, heightPx, cellSize, dpi);
 
     const loop = () => {
@@ -139,10 +143,10 @@ class GameOfLife extends HTMLCanvasElement {
       throw new Error("cannot init context for rendering");
     }
 
+    context.scale(dpi, dpi);
     context.clearRect(0, 0, width, height);
 
     return () => {
-      console.log("Loooooop");
       gameOfLife.grid.forEach((row, rowIndex) => {
         row.forEach((cell, cellIndex) => {
           if (cell === CellState.Alive) {
